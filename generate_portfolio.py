658b89e92067eaf37811345b6fc43e2373a9bb34
Jake Pullen <hello@jake-is.me>
2025-07-07 20:33:14 +0100

    fix: 🐛 Removed UV default file and re-added default social links

--- generate_portfolio.py
+++ generate_portfolio.py
@@ -11,11 +11,11 @@ with Path("portfolio.json").open(encoding="utf-8") as f:
 # Add any extra context if needed
 data["current_year"] = datetime.now(tz=UTC).year
 
-if "social_links" in data:
-    for link in data["social_links"]:
-        if link.get("svg_path"):
-            with Path(link["svg_path"]).open(encoding="utf-8") as svg_file:
-                link["svg_data"] = svg_file.read()
+for link in data.get("social_links"):
+    if not link.get("svg_path"):
+        continue
+    with Path(link["svg_path"]).open(encoding="utf-8") as svg_file:
+        link["svg_data"] = svg_file.read()
 
 # Set up Jinja environment
 env = Environment(loader=FileSystemLoader("."), autoescape=True)
@@ -25,10 +25,6 @@ resume_template = env.get_template("resume_template.html")
 # Render the template with the data
 html_output = index_template.render(**data)
 resume_output = resume_template.render(**data)
-
-# This is equivalent to...
-# html_output = index_template.render(name=data["name"], label=data["label"]...)
-# resume_output = resume_template.render(name=data["name"], label=data["label"]...)
 
 # Write the output to an HTML file
 with Path("index.html").open("w", encoding="utf-8") as f:
